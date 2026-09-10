71655967ee7bc8d13b9d718bef137d711211d773
lxien <yuanyicode@gmail.com>
2026-09-10 19:17:02 +0800

    chore: bump version to 3.6.0

--- docs/src/components/DownloadMatrix/index.tsx
+++ docs/src/components/DownloadMatrix/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.css';
 
 const REPO = 'orbien-org/orbien';
 
-const FALLBACK_VERSION = '3.6.0-beta.1';
+const FALLBACK_VERSION = '3.6.0';
 
 type OsId = 'windows' | 'linux' | 'darwin' | 'freebsd';
 type ArchId = 'amd64' | 'arm64';
