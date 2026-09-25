15e265e8737240f18262e2ebcebc1df373c750f2
lxien <yuanyicode@gmail.com>
2026-09-26 00:47:43 +0800

    chore: bump version to 3.7.0-beta.1

--- client-java/orbien-client/src/main/java/io/github/lxien/orbien/client/OrbienClient.java
+++ client-java/orbien-client/src/main/java/io/github/lxien/orbien/client/OrbienClient.java
@@ -30,7 +30,7 @@ import org.slf4j.LoggerFactory;
 
 public final class OrbienClient implements AutoCloseable {
     private static final Logger log = LoggerFactory.getLogger(OrbienClient.class);
-    private static final String VERSION = "3.6.1-beta.1";
+    private static final String VERSION = "3.7.0-beta.1";
 
     private final OrbienClientConfig config;
     private final AtomicBoolean started = new AtomicBoolean(false);
